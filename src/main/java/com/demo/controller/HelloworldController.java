fd65877ae855b7bc3c0994cbf4f6051520870a85
Manoj-kr <mroul914@gmail.com>
2025-04-03 11:45:31 +0530

    second update

--- src/main/java/com/demo/controller/HelloworldController.java
+++ src/main/java/com/demo/controller/HelloworldController.java
@@ -1,5 +1,13 @@
 package com.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloworldController {
 
+	@GetMapping
+	public String getMessage() {
+		return "hello_world";
+	}
 }
