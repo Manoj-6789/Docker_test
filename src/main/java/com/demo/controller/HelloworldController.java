bff38cf4a8c9355b982e61bf72a5d7ec6db6fd13
Manoj-kr <mroul914@gmail.com>
2025-04-08 18:26:39 +0530

    demo-docker

--- src/main/java/com/demo/controller/HelloworldController.java
+++ src/main/java/com/demo/controller/HelloworldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloworldController {
 
-	@GetMapping
+	@GetMapping("/message")
 	public String getMessage() {
 		return "hello_world";
 	}
